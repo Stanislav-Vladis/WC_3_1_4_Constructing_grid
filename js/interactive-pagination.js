154892e032678214c078ea03d7db41a721b56713
Stanislav Vladislavovich <stanislav.vladis@yandex.ru>
2023-12-15 20:31:07 +0300

    Fix after review

--- js/interactive-pagination.js
+++ js/interactive-pagination.js
@@ -1,15 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const paginationItems = document.querySelectorAll('.main__pagination-item');
+const paginationItems = document.querySelectorAll('.main__pagination-item');
+let activeItem;
 
-  paginationItems.forEach(function(item) {
-    item.addEventListener('click', function(event) {
-      // Удаляем класс у предыдущего активного элемента
-      const activeItem = document.querySelector('.main__pagination-item--active');
-      if (activeItem) {
-        activeItem.classList.remove('main__pagination-item--active');
-      }
-      // Добавляем класс к текущему элементу
-      item.classList.add('main__pagination-item--active');
-    });
+paginationItems.forEach(function (item) {
+  if (item.classList.contains('main__pagination-item--active')) activeItem = item;
+
+  item.addEventListener('click', function () {
+    // Удаляем класс у предыдущего активного элемента
+    if (activeItem) {
+      activeItem.classList.remove('main__pagination-item--active');
+    }
+    // Добавляем класс к текущему элементу
+    item.classList.add('main__pagination-item--active');
+    activeItem = item;
   });
 });
